2b7119e76c0b0588e2554386173f43972cb3f3a4
Parthsukhadiya18 <121274802+Parthsukhadiya18@users.noreply.github.com>
2024-12-03 14:17:28 +0530

    Add files via upload

---
+++ c++/01_basic.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    // printf
+    cout << "hi\n";
+    cout << "PArth ";
+    int x, y;
+    // scnf
+    cout << "enter value  x and y";
+    cin >> x >> y;
+    cout << "x value" << x;
+    cout << "y value" << y;
+    int z = x + y;
+    cout << "z value=" << z;
+    return 0;
+}
---
+++ c++/01_pattren.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // *****
+    // *****
+    // *****
+    // *****
+    // *****
+
+    //    for i row out ter loop
+    for (int i = 1; i <= 5; i++)
+    {
+
+        //     j inner loop for column
+        for (int j = 1; j <= 5; j++)
+        {
+            cout << "*";
+        }
+
+        cout << "\n";
+    }
+
+    return 0;
+}
